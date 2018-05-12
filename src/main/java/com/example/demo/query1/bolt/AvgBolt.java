524c99b53b7b03b3e6b865403beebd256bfe5027
cristian <miliacristian4@gmail.com>
2018-05-12 16:30:59 +0200

    query 1

--- src/main/java/com/example/demo/query1/bolt/AvgBolt.java
+++ src/main/java/com/example/demo/query1/bolt/AvgBolt.java
@@ -34,37 +34,37 @@ public class AvgBolt extends BaseWindowedBolt {
     public void execute(TupleWindow inputWindow) {
 
         System.out.println("/n/n");
-        List<Tuple> tupleList = inputWindow.get();
+        List<Tuple> tupleList = inputWindow.get();//ottieni la lista di tuple in finestra
         for ( Tuple t : tupleList){
-            Incrocio l = (Incrocio) t.getValueByField("incrocio");
-            if(mappa.containsKey(l.getId())){
-                mappa.put(l.getId(), processAvg(mappa.get(l.getId()),l));
+            Incrocio l = (Incrocio) t.getValueByField("incrocio");//ottieni l'incrocio,perchè id incrocio e non id?
+            if(mappa.containsKey(l.getId())){//se la mappa contiene l'incrocio
+                mappa.put(l.getId(), processAvg(mappa.get(l.getId()),l));//aggiorna la media
             }
-            else{
+            else{//la mappa non contiene l'incrocio,aggiungi nella mappa l'incrocio
                 mappa.put(l.getId(),l);
             }
         }
         System.out.println("/n/n");
-
-        List<Incrocio> classifica = createList(mappa);
+        //dalla mappa ogni avg bolt avrà vari incroci e li deve raggruppare per ottenere una classifica
+        List<Incrocio> classifica = createList(mappa);//crea la classifica
         System.out.println("/n/n");
 
         System.out.println(classifica);
-        collector.emit(new Values(classifica.get(0).getId(),new Rank(classifica)));
+        collector.emit(new Values(classifica.get(0).getId(),new Rank(classifica)));//emetti la classifica
     }
 
-    private List<Incrocio> createList(HashMap<Integer,Incrocio> mappa){
+    private List<Incrocio> createList(HashMap<Integer,Incrocio> mappa){//ritorna una lista di incroci da una hashmap
         List<Incrocio> classifica = new ArrayList<>();
         for (Incrocio i : mappa.values()) {
-            classifica.add(i);
+            classifica.add(i);//aggiungi l'incrocio nell hashmap
             System.out.println(i.getVelocitàMedia());
             System.out.println(i.getNumeroVeicoli());
-            mappa.remove(i);
+            mappa.remove(i);//rimuovi l'incrocio dall'hashmap
         }
         return classifica;
     }
 
-    private Incrocio processAvg(Incrocio oldi,Incrocio i){
+    private Incrocio processAvg(Incrocio oldi,Incrocio i){//aggiorna la media
         int nTot = oldi.getNumeroVeicoli()+i.getNumeroVeicoli();
         float app=oldi.getVelocitàMedia()*oldi.getNumeroVeicoli()+i.getVelocitàMedia()*i.getNumeroVeicoli();
         i.setVelocitàMedia(app/nTot);
