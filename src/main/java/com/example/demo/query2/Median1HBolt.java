28c425516e51638b2ab967bf03d45564bcad3295
cristian <miliacristian4@gmail.com>
2018-05-23 19:01:50 +0200

    query 2....

--- src/main/java/com/example/demo/query2/Median1HBolt.java
+++ src/main/java/com/example/demo/query2/Median1HBolt.java
@@ -2,6 +2,8 @@ package com.example.demo.query2;
 
 import com.example.demo.costant.Costant;
 import com.example.demo.query1.entity.Incrocio;
+import com.tdunning.math.stats.AVLTreeDigest;
+import com.tdunning.math.stats.TDigest;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -10,31 +12,51 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Median1HBolt extends BaseRichBolt {
 
-
+    private int count;//max 4
     private OutputCollector collector;
-    private HashMap<Integer, Incrocio> mappa;
+    private TDigest t;
+    private ArrayList<Incrocio> globalList;
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields(Costant.LIST_INTERSECTION));
+
     }
 
 
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         this.collector = collector;
-        mappa = new HashMap<>();
+        count=0;
+        t=new AVLTreeDigest(Costant.COMPRESSION);
+        globalList=new ArrayList<>();
     }
 
     @Override
     public void execute(Tuple input) {
-        collector.emit(new Values( new ArrayList<Incrocio>())  );
+        count++;
+        ArrayList<Incrocio>list=(ArrayList)input.getValueByField(Costant.LIST_INTERSECTION);
+        //unisci NUM_median15mbolt in un unica lista incroci
+        //ai successivi NUM_median15mbolt si uniscono le 2 liste
+        if(count%Costant.NUM_MEDIAN_15M_BOLT!=0) {
+            globalList.addAll(list);
+        }
+        else{
+
+        }
+        if(count==Costant.COUNT_MEDIAN_1H*Costant.NUM_MEDIAN_15M_BOLT){
+            count=0;
+            t=null;
+            t=new AVLTreeDigest(Costant.COMPRESSION);
+            collector.emit(new Values( new ArrayList<Incrocio>()));
+        }
         System.out.println("ho inviato dal Median1HBolt");
     }
 
